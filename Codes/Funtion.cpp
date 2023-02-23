7c8a7ab35fc9253c287351b63d02bba5a80ce145
shruti0312 <kamboj.shruti01@gmail.com>
2023-02-23 16:48:43 +0530

    initial commit

---
+++ Codes/Funtion.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int sum(int a, int b) {
+ int result = a + b;
+//  
+return result;
+}
+
+int main() {
+
+    int a,b;
+    cin >> a >> b;
+    
+   cout << sum(a,b) << endl;
+    cout << "Hello World";
+    return 0;
+}
